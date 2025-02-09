c2ddc673630930485a9d32ec7c8bfc53b848dd55
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-02-09 10:42:41 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import modal
+import json
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+def load_json_leaderboard():
+    volume = modal.Volume.from_name("face-database")
+    metadata = volume.read_file("metadata.json")
+    db = list(metadata)[0]
+    leaderboard = json.loads(db)
+    return leaderboard
+
+
+@app.get("/faces")
+async def get_faces():
+    return load_json_leaderboard()
